5f6f3e50b5edad448056718de8634a40c48e0abf
jarreedd <jarreedw@gmail.com>
2023-08-28 08:19:55 +0200

    linked bookmarks properly

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-// alert("Welcome to my website! Please note that this website is still in the development phase, so you might encounter some features that are a work in progress. Thank you for your understanding and patience!")
+alert("Welcome to my website! Please note that this website is still in the development phase, so you might encounter some features that are a work in progress. Thank you for your understanding and patience!")
 
 let meunOpen = false
 
