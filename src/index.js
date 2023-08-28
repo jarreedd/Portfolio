5a363ee452ebc0705206b6796e5f2d216aa0f633
jarreedd <jarreedw@gmail.com>
2023-08-28 07:56:24 +0200

    toggle meun functionality

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-alert("Welcome to my website! Please note that this website is still in the development phase, so you might encounter some features that are a work in progress. Thank you for your understanding and patience!")
+// alert("Welcome to my website! Please note that this website is still in the development phase, so you might encounter some features that are a work in progress. Thank you for your understanding and patience!")
+
+let meunOpen = false
+
+const menuBtn = document.querySelector('.menu-btn')
+const menuItems = document.querySelector('.menu-items')
+
+function toggleMenu() {
+    meunOpen = !meunOpen
+
+    if (meunOpen) {
+        menuItems.classList.add('show')
+    } else {
+        menuItems.classList.remove('show')
+    }
+}
+
+menuBtn.addEventListener('click', toggleMenu)
+menuItems.addEventListener('click', toggleMenu)
