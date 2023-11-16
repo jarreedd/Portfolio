099262f1fdda7ce162ef7da9c1cfb1bd65988ef7
jarreedd <jarreedw@gmail.com>
2023-11-16 10:49:42 +0200

    request resume functionality

--- src/index.js
+++ src/index.js
@@ -1,18 +1,38 @@
-let meunOpen = false
+let isMeunOpen = false
+let isShowOverlay = false
 
 const menuBtn = document.querySelector('.menu-btn')
 const menuItems = document.querySelector('.menu-items')
-const resumeRequest = document.querySelector('.resume-request')
+const resumeRequestBtn = document.querySelector('.resume-request')
+const closeFormBtn = document.querySelector('.close-form')
+const overlay =document.querySelector('.overlay')
 
 function toggleMenu() {
-    meunOpen = !meunOpen
+    isMeunOpen = !isMeunOpen
 
-    if (meunOpen) {
+    if (isMeunOpen) {
         menuItems.classList.add('show')
     } else {
         menuItems.classList.remove('show')
     }
 }
 
+function toggleOverlay() {
+    isShowOverlay = !isShowOverlay
+
+    if (isShowOverlay) {
+        overlay.classList.add('show')
+    } else {
+        overlay.classList.remove('show')
+    }
+}
+
+function toggleRequestForm() {
+    toggleOverlay()
+}
+
 menuBtn.addEventListener('click', toggleMenu)
 menuItems.addEventListener('click', toggleMenu)
+
+resumeRequestBtn.addEventListener('click', toggleRequestForm)
+closeFormBtn.addEventListener('click', toggleRequestForm)
